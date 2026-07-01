78754c00c0706af74edff49ae9aced15602aec86
Tamminga Budds <budds300@gmail.com>
2026-07-01 20:02:10 +0300

    logs update

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -1,5 +1,10 @@
 import Medusa from '@medusajs/js-sdk';
 
+
+console.log('API URL:', process.env['NEXT_PUBLIC_API_URL']);
+console.log('Key present:', !!process.env['NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY']);
+
+
 export const sdk = new Medusa({
   baseUrl: process.env['NEXT_PUBLIC_API_URL'] ?? 'http://localhost:9000',
   publishableKey: process.env['NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY'] ?? '',
